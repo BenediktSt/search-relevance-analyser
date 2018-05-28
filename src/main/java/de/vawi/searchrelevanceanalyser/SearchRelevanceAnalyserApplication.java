101aee1d16fc8ddbf59caa81ff3fabb0d93a56f5
Benedikt Straube <benediktstraube@hotmail.de>
2018-05-28 21:55:21 +0200

    initial commit

---
+++ src/main/java/de/vawi/searchrelevanceanalyser/SearchRelevanceAnalyserApplication.java
@@ -0,0 +1,12 @@
+package de.vawi.searchrelevanceanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchRelevanceAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchRelevanceAnalyserApplication.class, args);
+	}
+}
